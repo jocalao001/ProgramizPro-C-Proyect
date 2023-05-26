4a4aa9f472009c2a3a4d171648fc377f37dbc9a5
jocalao001 <joca-001@hotmail.com>
2023-05-25 20:23:44 -0700

    25MAY23

---
+++ strings/countCharacters.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    char text[100];
+    
+    // get input string
+    fgets(text, sizeof(text), stdin);
+
+    // variable to count number of characters
+    int count = 0;
+
+    // use while loop with condition
+    // if first character of text is not equal to '\0'
+    while(text[count] != '\0'){
+        ++count;
+    }
+
+    // print count
+    printf("%d\n", count);
+
+    return 0;
+}
